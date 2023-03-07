1f02c03eef4fbabe5ac9ca4b16ab813a15620204
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-03-06 21:39:30 -0500

    Completed

--- test/indexTest.js
+++ test/indexTest.js
@@ -13,6 +13,7 @@ describe('index.js', function () {
 
       cats.push('Milo', 'Otis', 'Garfield');
     });
+  
 
     describe('destructivelyAppendCat(name)', function () {
       it('appends a cat to the end of the cats array', function () {
